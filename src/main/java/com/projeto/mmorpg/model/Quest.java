ac18cf543323323602dc8ec83b2cd33048b875c6
Cauã Loch <88676881+CauaLoch9860@users.noreply.github.com>
2024-09-28 15:29:37 -0300

    Update Quest.java

--- src/main/java/com/projeto/mmorpg/model/Quest.java
+++ src/main/java/com/projeto/mmorpg/model/Quest.java
@@ -4,14 +4,14 @@ public class Quest {
     private String id;
     private String title;
     private String description;
-    private String hint; // Se preferir, você pode manter o hint como atributo da quest.
+    private String hint;
 
     // Construtor
     public Quest(String id, String title, String description, String hint) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.hint = hint; // Aqui, você pode usar o hint como atributo da quest.
+        this.hint = hint; 
     }
 
     // Getters e Setters
